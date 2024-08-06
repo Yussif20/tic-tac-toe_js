b925ae16d881e13ae98b6d4a5949e5e5b363b841
Yousef ayman <yousefayman2000500@gmail.com>
2024-08-06 20:13:50 +0300

    cell click event pt.1

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 let currentPlayer = "X";
-const NUMBER_OF_ROWS = 4;
+const NUMBER_OF_ROWS = 3;
 const turns = NUMBER_OF_ROWS ** 2;
 let turnsCounter = 0;
+
+const cellClickHandler = (event,index) =>{
+    const cell = event.target;
+    console.log(cell)
+    console.log(index)
+    const row = Math.floor(index / NUMBER_OF_ROWS);
+    const col = index % NUMBER_OF_ROWS
+    console.log({row})
+    console.log({col})
+}
 
 const createBoard =()=>{
     const container = document.querySelector('.container');
@@ -12,7 +22,7 @@ const createBoard =()=>{
         const cellElementString = `<div class="cell"><span class="value"></span></div>`
         const cellElement = document.createRange().createContextualFragment(cellElementString)
 
-        cellElement.querySelector(".cell").onclick = (event)=>console.log("you clicked me")
+        cellElement.querySelector(".cell").onclick = (event)=>cellClickHandler(event,i)
         board.appendChild(cellElement);
 
         document.documentElement.style.setProperty("--grid-rows",NUMBER_OF_ROWS)
